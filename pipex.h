bc178c83038aacc12ee49c2853d6508d1de731b2
vcastilh <vcastilh@student.42sp.org.br>
2022-04-22 02:37:48 +0200

    feat: Add header file

---
+++ pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 00:23:02 by vcastilh          #+#    #+#             */
+/*   Updated: 2022/04/22 00:23:04 by vcastilh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include "libft.h"
+#endif
