6209da109ac8ebe0fa7b4dd0a60122c7071a2f16
vcastilh <vcastilh@student.42sp.org.br>
2022-04-29 23:04:49 +0200

    refactor: Add get_bin_path()

--- main.c
+++ main.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:49:54 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/04/20 18:41:55 by vcastilh         ###   ########.fr       */
+/*   Updated: 2022/04/29 23:02:16 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char	**get_bin_path(char **envp)
+{
+	char	**bin_path;
+	size_t	i;
+	char	*tmp_bin_path;
+
+	bin_path = ft_split(ft_getenv("PATH", envp), ':');
+	i = 0;
+	while (bin_path[i])
+	{
+		tmp_bin_path = bin_path[i];
+		bin_path[i] = ft_strjoin(bin_path[i], "/");
+		free(tmp_bin_path);
+		tmp_bin_path = NULL;
+		i++;
+	}
+	return (bin_path);
+}
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -19,7 +38,6 @@ int	main(int argc, char *argv[], char *envp[])
 	char	*cmd2_path = "/usr/bin/wc";
 	char	*cmd2_args[] = {"wc", NULL};
 	char	**bin_path;
-	char	*tmp_bin_path;
 	int		pid;
 	int		pipe_fd[2];
 	int		fd[2];
@@ -27,17 +45,7 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (argc != 5)
 		return (1);
-	bin_path = ft_split(ft_getenv("PATH", envp), ':');
-	i = 0;
-	while (bin_path[i])
-	{
-		tmp_bin_path = bin_path[i];
-		bin_path[i] = ft_strjoin(bin_path[i], "/");
-		printf("parcial bin: %s\n", bin_path[i]);
-		free(tmp_bin_path);
-		tmp_bin_path = NULL;
-		i++;
-	}
+	bin_path = get_bin_path(envp); 
 	printf("argv: %s\n", argv[0]);
 	if(pipe(pipe_fd) == -1)
 		return (1);
