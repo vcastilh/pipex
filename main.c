dd26bfd0c17f077e9575c6c1d0927c5bdbe69c6f
vcastilh <vcastilh@student.42sp.org.br>
2022-05-12 06:17:32 +0200

    refactor: add data struct

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:49:54 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/05/11 20:47:34 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/12 05:55:27 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,55 +28,53 @@ void	free_ptr(char **str)
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	**bin_path;
-	char	**cmd_argv;
 	int		pid;
 	int		pipe_fd[2];//read_end[0] write_end[1]
 	int		fd[2];//fd[0] = file1   fd[1] = file2
 	size_t	i;
-	int		cmd_pos;
+	t_data	data;
 
-	cmd_pos = 1;
-	while (++cmd_pos < argc - 1) 
+	data.cmd_pos = 1;
+	while (++data.cmd_pos < argc - 1) 
 	{
 		if(pipe(pipe_fd) == -1)
 			return (1);
-		cmd_argv = get_cmd_argv(argv[cmd_pos]); //{"cat", NULL}; {"wc", NULL};
-		bin_path = get_bin_path(envp, cmd_argv); // "/usr/bin/cat" ; {"cat", NULL};
+		data.cmd_argv = get_cmd_argv(argv[data.cmd_pos]); //{"cat", NULL}; {"wc", NULL};
+		data.bin_path = get_bin_path(envp, data.cmd_argv); // "/usr/bin/cat" ; {"cat", NULL};
 		i = 0;
-		while (bin_path[i] != NULL)
+		while (data.bin_path[i] != NULL)
 		{
-			if (!(access(bin_path[i], F_OK | X_OK)))
+			if (!(access(data.bin_path[i], F_OK | X_OK)))
 			{
 				pid = fork();
 				if (pid < 0)
 					return (1);
 				if (pid == 0)
 				{
-					if (cmd_pos == 2)
+					if (data.cmd_pos == 2)
 					{
 						fd[0] = open("file1.txt", O_RDONLY);//fd deve existir para o segundo comando tbm(segundo cmd precisa de input?)
 						close(pipe_fd[0]);
 						dup2(fd[0], STDIN_FILENO);
 						dup2(pipe_fd[1], STDOUT_FILENO);
 						close(pipe_fd[1]);
-						if(execve(bin_path[i], cmd_argv, envp) == -1)
+						if(execve(data.bin_path[i], data.cmd_argv, envp) == -1)
 							return (1);
 					}
-					else if (cmd_pos == argc - 2)
+					else if (data.cmd_pos == argc - 2)
 					{
 						fd[1] = open("file2.txt", O_RDWR | O_TRUNC | O_CREAT, 0664);
 						close(pipe_fd[0]);
 						close(pipe_fd[1]);
 						dup2(fd[1], STDOUT_FILENO);
-						if(execve(bin_path[i], cmd_argv, envp) == -1)
+						if(execve(data.bin_path[i], data.cmd_argv, envp) == -1)
 							return (1);
 					}
 					else
 					{
 						close(pipe_fd[0]);
 						dup2(pipe_fd[1], STDOUT_FILENO);
-						if(execve(bin_path[i], cmd_argv, envp) == -1)
+						if(execve(data.bin_path[i], data.cmd_argv, envp) == -1)
 							return (1);
 					}
 				}
@@ -88,8 +86,8 @@ int	main(int argc, char *argv[], char *envp[])
 		close(pipe_fd[1]);
 		dup2(pipe_fd[0], STDIN_FILENO);
 		close(pipe_fd[0]);
-		free_ptr(cmd_argv);
-		free_ptr(bin_path);
+		free_ptr(data.cmd_argv);
+		free_ptr(data.bin_path);
 	}
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 00:23:02 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/05/11 20:46:46 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/12 05:56:12 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,10 @@
 # include "libft.h"
 char	**get_cmd_argv(char *argv);
 char	**get_bin_path(char **envp, char **cmd_argv);
+typedef struct s_data
+{
+	char	**bin_path;
+	char	**cmd_argv;
+	int		cmd_pos;
+}	t_data;
 #endif
