a39837cbf75855109b7706be0702176eee4c4fa0
vcastilh <vcastilh@student.42sp.org.br>
2022-05-27 22:43:40 +0200

    refactor dojo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:49:54 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/05/27 20:28:11 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/27 22:38:31 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	main(int argc, char *argv[], char *envp[])
 		handle_exit(argv[0], ft_strdup(":Error: need 4 arguments\n"), &data, 0);
 	data.fd[0] = open_file(argv[1], F_OK, &data, 0);
 	data.cmd_pos = 1;
+	if (pipe(data.pipe_fd) == -1)
+		handle_exit(argv[0], ft_strdup(":Pipe error\n"), &data, 0);
 	while (++data.cmd_pos < argc - 1)
 	{
-		if (pipe(data.pipe_fd) == -1)
-			handle_exit(argv[0], ft_strdup(":Pipe error\n"), &data, 0);
 		get_data(&data, argv, envp);
 		data.pid = fork();
 		if (data.pid < 0)
@@ -31,13 +31,17 @@ int	main(int argc, char *argv[], char *envp[])
 		else if (data.pid == 0)
 			child_process(&data, argv, envp);
 		waitpid(data.pid, NULL, 0);
-		close(data.pipe_fd[1]);
-		dup2(data.pipe_fd[0], STDIN_FILENO);
-		close(data.pipe_fd[0]);
+		//close(data.pipe_fd[1]);
+		////dup2(data.pipe_fd[0], STDIN_FILENO);
+		//close(data.pipe_fd[0]);
 		free_ptr(data.cmd_argv);
 		free_ptr(data.bin_path);
 		free(data.pathname);
 	}
+	close(data.pipe_fd[1]);
+	////dup2(data.pipe_fd[0], STDIN_FILENO);
+	close(data.pipe_fd[0]);
+	close(data.fd[0]);
 	close(data.fd[1]);
 	return (0);
 }
