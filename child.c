014e72d4172c0d87c33ba1a6a82c46c35a31c274
vcastilh <vcastilh@student.42sp.org.br>
2022-05-28 18:07:17 +0200

    fix: close()

--- child.c
+++ child.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 01:59:22 by coder             #+#    #+#             */
-/*   Updated: 2022/05/28 04:31:42 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/28 18:04:56 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	child_process(t_data *data, char *argv[], char *envp[])
 	{
 		close(data->pipe_fd[0]);
 		dup2(data->pipe_fd[1], STDOUT_FILENO);
-		close(data->pipe_fd[1]);
+		//close(data->pipe_fd[1]);
 		if(dup2(data->fd[0], STDIN_FILENO) == -1)
 		{
 			strerror(2);
@@ -37,7 +37,7 @@ void	child_process(t_data *data, char *argv[], char *envp[])
 	{
 		data->fd[1] = open_file(argv[4],  O_TRUNC | O_RDWR | O_CREAT, data, 0);
 		dup2(data->pipe_fd[0], STDIN_FILENO);
-		close(data->pipe_fd[0]);
+		//close(data->pipe_fd[0]);
 		dup2(data->fd[1], STDOUT_FILENO);
 		close(data->fd[1]);
 		if (execve(data->pathname, data->cmd_argv, envp) == -1)
